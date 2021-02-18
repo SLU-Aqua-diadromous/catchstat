bdc8b179f1b824730eeba6545181ebff7d709a4a
Anders Kagervall <anders.kagervall@gmail.com>
2021-02-18 09:24:25 +0100

    Data sets "gear" and "species"

---
+++ R/datasets-documentation.R
@@ -0,0 +1,33 @@
+### Docs data.frame "gear" ############################################
+#' Names, code number and miscellaneous data of gear types
+#'
+#' A dataset describing fishing gear used in data files with catches reported
+#' to HaV from commercial fishermen.
+#'
+#' @format A data frame with 10 variables:
+#' \describe{
+#'   \item{REDSKAP}{gear name in Swedish}
+#'   \item{REDSKAPENG}{gear name in English}
+#'   \item{REDSKKOD}{numeric code for gear}
+#'   \item{AP}{Gear is "Passivt" or "Aktivt"}
+#'   \item{EUKODNY}{Some other classification}
+#'   \item{FR}{Gear is "Fast" or "Rörligt"}
+#'   \item{FAOKODNY}{Some other classification}
+#' }
+"gear"
+
+### Docs data.frame "species2 ############################################
+#' Names, code number and miscellaneous data of gear types
+#'
+#' A dataset with commercially fished species. Each species is
+#' described with a three character long code (MAF/FAO), english
+#' common name and swedish common name.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'   \item{MAFKOD}{a three charatcter long code (MAF/FAO), E.g. SAL and TRS}
+#'   \item{Species}{English common name, e.g. Salmon and Trout}
+#'   \item{Art}{Swedish common name, e.g. Lax and Öring}
+#' }
+"species"
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,7 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
+## code to prepare 'gear' dataset
+gear <- readxl::read_excel("data-raw/gear.xlsx")
+usethis::use_data(gear, overwrite = TRUE)
+## code to prepare 'gear' dataset
+species <- readxl::read_excel("data-raw/species.xlsx")
+usethis::use_data(species, overwrite = TRUE)
+##
