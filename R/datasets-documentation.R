dbb59a0d438d622053e761baecd7e74924fbe637
Anders Kagervall <anders.kagervall@gmail.com>
2025-01-16 16:34:07 +0100

    Update script to build data-raw

--- R/datasets-documentation.R
+++ R/datasets-documentation.R
@@ -16,7 +16,7 @@
 #' }
 "gear"
 
-### Docs data.frame "species2 ############################################
+### Docs data.frame "species" ############################################
 #' Names, code number and miscellaneous data of gear types
 #'
 #' A dataset with commercially fished species. Each species is
@@ -45,3 +45,23 @@
 #'   \item{TEXT}{Description in Swedish}
 #' }
 "ackflagg"
+
+#' Table to map SUBDIV to FishingArea
+#'
+#' \describe{
+#' \item{SUBDIV}{character, ICES subdivisions in the Baltic sea}
+#' \item{FishingArea}{character, corresponding fishing area used in Intercatch}
+#' }
+"fishing_area"
+
+#' Table to map HaV REDSKKOD to WGBAST metier
+#'
+#' \describe{
+#' \item{metier}{character, metier used in Intercatch defined by WGBAST}
+#' \item{gear}{character, gear name in Swedish defined by HaV}
+#' \item{gear_code}{numeric, gear code defined by HaV}
+#' \item{UnitEffort}{character, code for the unit for effort used for this gear type. Used in Intercatch (defined by WGBAST)}
+#' }
+"fleet_key"
+
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,28 @@
 ## code to prepare 'gear' dataset
 gear <- readxl::read_excel("data-raw/gear.xlsx")
 usethis::use_data(gear, overwrite = TRUE)
+
 ## code to prepare 'gear' dataset
 species <- readxl::read_excel("data-raw/species.xlsx")
 usethis::use_data(species, overwrite = TRUE)
+
 ##
 ## code to prepare 'ackflagg' dataset
 ackflagg <- readxl::read_excel("data-raw/ackflagg.xlsx")
 usethis::use_data(ackflagg, overwrite = TRUE)
+
+##
+## code to prepare 'fleet_key' dataset
+fleet_key <- readxl::read_excel("data-raw/fleet_key.xlsx")
+usethis::use_data(fleet_key, overwrite = TRUE)
+
+##
+## code to prepare 'fishing_area' dataset
+fishing_area <- data.frame(
+  SUBDIV = c("22", "23", "24", "25", "26", "27", "28", "281", "282", "29", "29N",
+             "29S", "30", "31", "32"),
+  FishingArea = c("27.3.c.22", "27.3.b.23", "27.3.d.24", "27.3.d.25", "27.3.d.26",
+                  "27.3.d.27", "27.3.d.28", "27.3.d.28", "27.3.d.28", "27.3.d.29",
+                  "27.3.d.29", "27.3.d.29", "27.3.d.30", "27.3.d.31", "27.3.d.32"),
+  stringsAsFactors = FALSE)
+usethis::use_data(fishing_area, overwrite = TRUE)
